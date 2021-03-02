eb2482abf17d21922c1c315093250f4f5dc38b48
Arielle Scherr <ariellescherr@gmail.com>
2021-03-02 15:30:45 -0600

    First commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import './Header.css';
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header">
+
+      <h2 className="page-title">Unsplash Stock Photo Search</h2>
+
+    </div>
+  )
+};
+
+export default Header;
